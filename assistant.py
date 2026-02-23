43fc83d6ecee5a915ee34abf7c3b3b6a44eecf2f
YatriJoshi <yatri.b.joshi@gmail.com>
2026-02-23 12:39:51 +0530

    Improved command handling using keyword detection

--- assistant.py
+++ assistant.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 
 
 def greet_user():
@@ -6,7 +7,37 @@ def greet_user():
     print(f"Welcome {name} to the Assistant Program!")
     return name
 
-
+def handle_commands(user_input):
+    if "time" in user_input:
+        now = datetime.datetime.now()
+        print("Assistant: The current time is", now.strftime("%H:%M:%S"))
+        return True
+    
+    elif "date" in user_input:
+        today = datetime.date.today()
+        print("Assistant: Today's date is", today)
+        return True
+    
+    elif user_input.startswith("add"):
+        parts = user_input.split()
+        
+        if len(parts) == 3 and parts[1].isdigit() and parts[2].isdigit():
+            result = int(parts[1] + int(parts[2]))
+            print("Assistant: Result is", result)
+        else:
+            print("Assistant: Usage: add 5 10")
+        return True
+    
+    elif user_input == "help":
+        print("Assistant: Available commands:")
+        print("- time -> Show current time")
+        print("- date -> Show today's date")
+        print("- add <num1> <num2> -> add numbers")
+        print("-bye -> exit assistant")
+        return True
+    
+    return False  
+           
 def run_assistant():
     greetings = ["hi", "hello", "hey", "whats up"]
     mood_questions = ["how are you", "how r you", "how r u"]
@@ -22,7 +53,12 @@ def run_assistant():
 
     while True:
         user = input("You: ").strip().lower()
-
+        
+        #1.Commands
+        if handle_commands(user):
+            continue
+        
+        #2. Conversation flow
         if user in greetings:
             print(f"Assistant: Hello, {name}!")
 
